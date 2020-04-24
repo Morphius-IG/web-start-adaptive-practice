1a32c9f860b32343e23c30eb4adfa0aa66972747
unknown <justlikeme11@mail.ru>
2020-04-24 16:03:24 +0200

    Добавил автоматическое закрытие всплывающего окна через 5 секунд

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,16 @@ var close = document.querySelector('#close');
 
 button.addEventListener('click', function() {
   modal.classList.add('modal_active');
+  setTimeout(function() {
+    modal.classList.remove('modal_active');
+  }, 3000);
+
   });
 
 close.addEventListener('click', function() {
     modal.classList.remove('modal_active');
   });
+
+
+
+
