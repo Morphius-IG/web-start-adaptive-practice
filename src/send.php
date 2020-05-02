a8cb72a0f1fb5d21dccd049e92fd80060231d4a6
unknown <justlikeme11@mail.ru>
2020-05-02 19:11:43 +0200

    Поменял расширение индекс

--- src/send.php
+++ src/send.php
@@ -12,7 +12,7 @@ $phone = urldecode($phone);
 
 $name = trim($name);
 $phone = trim($phone);
-
+git status
 
 if (mail("justlikeme11@mail.ru", "Заявка с сайта", "Имя:".$name.". Телефон: ".$phone ,"From: justlikeme11@mail.ru \r\n"))
  {
